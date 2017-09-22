d650af2929dd84b9dd2f4142fc8415e4687cbf7b
Leonardo Cidral <leonardo.cidral@dotgroup.com.br>
2017-09-22 19:15:22 -0300

    Remove external container

--- src/widgets/views/quote.php
+++ src/widgets/views/quote.php
@@ -1,14 +1,10 @@
 <div class="container">
-    <div class="row">
-        <div class="container">
-            <blockquote class="quote-card <?= $color ?>">
-                <p>
-                    <?= $quote->quoteText ?>
-                </p>
-                <cite>
-                    <?= $quote->quoteAuthor ?>
-                </cite>
-            </blockquote>
-        </div>
-    </div>
+    <blockquote class="quote-card <?= $color ?>">
+        <p>
+            <?= $quote->quoteText ?>
+        </p>
+        <cite>
+            <?= $quote->quoteAuthor ?>
+        </cite>
+    </blockquote>
 </div>
